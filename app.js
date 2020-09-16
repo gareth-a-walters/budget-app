643215f8a1e6ac63aa7884e9582ebfd162626c6a
Gareth-Walters <gareth.a.walters@gmail.com>
2020-09-16 12:08:49 +0100

    Implemented module pattern

---
+++ app.js
@@ -0,0 +1,23 @@
+//BUDGET CONTROLLER
+let budgetController = (function(){
+
+    //some code
+
+})();
+
+
+//UI CONTROLLER 
+let UIController = (function(){
+
+    //some code
+
+});
+
+
+
+//GLOBAL APP CONTROLLER
+let controller = (function(budgetCtrl, UICtrl){
+
+    //some code 
+
+})(budgetController, UIController);
