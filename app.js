4f55c6e13aaa5c952681dec637e4831fb59c8128
Gareth-Walters <gareth.a.walters@gmail.com>
2020-09-16 12:49:21 +0100

    Reading user input data

--- app.js
+++ app.js
@@ -8,29 +8,56 @@ let budgetController = (function(){
 //UI CONTROLLER 
 let UIController = (function(){
 
-    //some code
+    let DOMstrings = {
+        inputType: '.add__type', 
+        inputDescription: '.add__description', 
+        inputValue: '.add__value', 
+        inputBtn: '.add__btn'
+    };
 
-});
+    return {
+        getInput: function(){
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //either exp or inc
+                description: document.querySelector(DOMstrings.inputDescription).value, 
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    };
+})();
 
 //GLOBAL APP CONTROLLER
 let controller = (function(budgetCtrl, UICtrl){
 
+    let DOM = UICtrl.getDOMstrings();
+
     let ctrlAddItem = function(){
+
         //1. Get field input data
+        let input = UICtrl.getInput();
+        console.log(input);
+
         //2. Add item to budget controller
+
         //3. Add item to UI
+
         //4. Calcualte the budget
-        //5. Display budget on UI   
-        console.log('working');
+
+        //5. Display budget on UI
+
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event){
         if(event.keyCode === 13 || event.which === 13){
             event.preventDefault(); // prevents the enter key from also triggering a click event
             ctrlAddItem();
         }
-    })
+    });
 
 })(budgetController, UIController);
