7cb247b444511f8b1dd28bd34595562c0527eb90
Gareth-Walters <gareth.a.walters@gmail.com>
2020-09-16 12:21:56 +0100

    Setup first event listener

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ let budgetController = (function(){
 
 })();
 
-
 //UI CONTROLLER 
 let UIController = (function(){
 
@@ -13,11 +12,25 @@ let UIController = (function(){
 
 });
 
-
-
 //GLOBAL APP CONTROLLER
 let controller = (function(budgetCtrl, UICtrl){
 
-    //some code 
+    let ctrlAddItem = function(){
+        //1. Get field input data
+        //2. Add item to budget controller
+        //3. Add item to UI
+        //4. Calcualte the budget
+        //5. Display budget on UI   
+        console.log('working');
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+        if(event.keyCode === 13 || event.which === 13){
+            event.preventDefault(); // prevents the enter key from also triggering a click event
+            ctrlAddItem();
+        }
+    })
 
 })(budgetController, UIController);
